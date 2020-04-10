2432db76e37b9775de2455a6de429e616702cdfb
duanruifeng <duanruifeng@sxylzinfo.com>
2020-04-10 18:51:49 +0800

    GitHubDemo练习

---
+++ githubdemo/src/main/java/cn/duanrf/githubdemo/GitHubDemo.java
@@ -0,0 +1,13 @@
+package cn.duanrf.githubdemo;
+
+/**
+ * @ClassName GitHubDemo
+ * @Description TODO
+ * @Author DRF
+ * @Date 2020/4/10 18:47
+ */
+public class GitHubDemo {
+    public static void main(String[] args) {
+        System.out.printf("This is a GitHubDemo!!");
+    }
+}
